586138fa1d488a9d2da39f9578c8f2151876f214
Muhammad Imran <mimran.646726@gmail.com>
2018-09-11 20:56:49 +0200

    changed dockerfile. added modules using pip

---
+++ graph_service_interface_schema/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='graphserviceschema',
+    version='0.1',
+    packages=['graphserviceschema'],
+    license='GPL v3.0'
+)
---
+++ openstack_querier/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='openstackqueryapi',
+    version='0.1',
+    packages=['openstackqueryapi'],
+    license='GPL v3.0'
+)
