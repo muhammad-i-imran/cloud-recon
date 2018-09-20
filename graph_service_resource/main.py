a32a0db665b8a0ef6327b66707088a8263ecbf5c
Muhammad Imran <mimran.646726@gmail.com>
2018-09-20 14:15:54 +0200

    added option to use differnt port to run service

--- graph_service_resource/main.py
+++ graph_service_resource/main.py
@@ -1,6 +1,30 @@
 from neo4jresource import app
-import os
+import sys, getopt
+
+def usage():
+    print("Usage: python main.py -a|--host <host> -p|--port <port>")
+
+def main(argv):
+    app.debug = True
+
+    host = '0.0.0.0'
+    port =  15135
+    try:
+        opts, args = getopt.getopt(argv, 'ha:p:', ['help',"host=", "port="])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt in ('-a', '--host'):
+            host = arg
+        elif opt in ('-p', '--port'):
+            port = int(arg)
+
+    app.run(host=host, port=port)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    main(sys.argv[1:])
