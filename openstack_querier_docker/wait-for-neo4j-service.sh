852017a34cf1f4ba68dabf2ebadf12d65f4c0bc2
Muhammad Imran <mimran.646726@gmail.com>
2018-11-16 22:38:15 +0100

    bug fix

--- openstack_querier_docker/wait-for-neo4j-service.sh
+++ openstack_querier_docker/wait-for-neo4j-service.sh
@@ -6,14 +6,13 @@ host_name="$1"
 shift
 port="$1"
 shift
-
 overridden_command="$@"
 
-while ! nc -z "$host_name" "$port"; do   
+until $(nc -z "$host_name" "$port"); do   
   >&2 echo "Neo4J is not available yet. Waiting for three second."
   sleep 3
 done
 
->&2 echo "Neo4J is available now. Executing the default command."
-exec "$overridden_command"
+>&2 echo "Neo4J Service is available now. Executing the default command."
+exec $overridden_command
 
