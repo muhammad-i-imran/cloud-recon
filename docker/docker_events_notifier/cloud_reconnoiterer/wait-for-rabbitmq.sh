556933b8500dc71c32d906ee53a7b346e2b4f400
Muhammad Imran <muhammad.imran1@protonmail.ch>
2019-03-26 17:43:03 +0100

    bug fixes for docker images

---
+++ docker/docker_events_notifier/cloud_reconnoiterer/wait-for-rabbitmq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+host_name="$1"
+shift
+port="$1"
+shift
+overridden_command="$@"
+
+until $(nc -z "$host_name" "$port"); do   
+  >&2 echo "RabbitMQ is not available yet. Waiting for three second."
+  sleep 3
+done
+
+>&2 echo "RabbitMQ Service is available now. Executing the default command."
+eval $overridden_command
+
