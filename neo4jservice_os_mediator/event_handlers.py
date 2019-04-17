b3eefa5af80629ae1af90138898e4568f2778137
Muhammad Imran <muhammad.imran1@protonmail.ch>
2019-04-17 12:22:38 +0200

    bug fixes

--- neo4jservice_os_mediator/event_handlers.py
+++ neo4jservice_os_mediator/event_handlers.py
@@ -26,16 +26,36 @@ def notifier_callback(event_type, payload):
         logger.error("Exception occured while reading event type from event_component_mappings: {0}".format(str(ex)))
     else:
         event_component_type = event_info['component']  # e.g. SERVERS, IMAGES, etc.
-        graph_element = event_info['graph_element']  # e.g. N for Node, R for relationship, NR for both.
+        graph_element = event_info['graph_element']  # e.g. N for Node, R for relationship. type: list
         event_operation = event_info['operation']  # e.g. U: update, D:delete, C: create
         component_id_property_in_payload = event_info['component_id_property_in_payload']
         if event_operation == 'D':
             data = {}
             try:
-                data['node_type'] = event_component_type
-                data['node_properties'] = payload[component_id_property_in_payload]
-                logger.info("Trying to delete node (node type: {0}, node id: {1}) in graph.".format(data['node_type'], data['node_properties']))
-                NodeManager.delete_nodes(data)
+                if  "N" in graph_element:
+                    data['node_type'] = event_component_type
+                    data['node_properties'] = payload[component_id_property_in_payload]
+                    logger.info("Trying to delete node (node type: {0}, node id: {1}) in graph.".format(data['node_type'], data['node_properties']))
+                    NodeManager.delete_nodes(data)
+                # if "R" in graph_element:
+                #     for relatiionship in event_info["relationships"]:
+                #         logger.info("Trying to create/update relationship(s) among nodes.")
+                #         source_node_properties = {}  # source node is the node for which the event is currently received
+                #         source_node_properties[relatiionship["target_property_name_in_db"]] = payload[
+                #             component_id_property_in_payload]
+                #
+                #         target_node_properties = {}  # target node is the node which is affect by the the event is currently received
+                #         target_node_properties[relatiionship["target_node_property_in_db"]] = payload[
+                #             relatiionship["target_node_type_id_in_payload"]]
+                #
+                #         relationship_data = {}
+                #         relationship_data["source_node_type"] = event_component_type
+                #         relationship_data["source_node_properties"] = source_node_properties
+                #         relationship_data["target_node_type"] = relatiionship['related_node_type']
+                #         relationship_data["target_node_properties"] = target_node_properties
+                #         relationship_data["relationship"] = relatiionship["relationship"]
+                #         relationship_data["relationship_properties"] = relatiionship["relationship_properties"]
+                #         RelationshipManager.delete_relationship(relationship_data)
             except KeyError as err:
                 logger.error("KeyError occured while reading key ({0}) from payload".format(str(err)))
             except Exception as ex:
@@ -43,7 +63,7 @@ def notifier_callback(event_type, payload):
         elif event_operation == 'C' or event_operation == 'U':
             node_type = event_component_type  # get corresponding label
             try:
-                if "N" in graph_element:
+                if "N" in graph_element: # if the event affects node
                     search_opts = {}
                     search_opts[cloud_config_info[node_type]['id_key']] = payload[
                         component_id_property_in_payload]  # query only that single element
@@ -53,22 +73,23 @@ def notifier_callback(event_type, payload):
                     logger.info("Trying to create or update node in graph.")
                     function_to_call(node_type=node_type, node_secondary_labels=cloud_config_info[node_type]['node_secondary_labels'],  label_key=cloud_config_info[node_type]['name_attr'],
                                      id_key=cloud_config_info[node_type]['id_key'], search_opts=search_opts)
-                if "R" in graph_element:
-                    for relatiionship in event_info["relationships"]:
+                if "R" in graph_element: # if the event affects relationship between nodes
+                    for relationship in event_info["relationships"]:
                         logger.info("Trying to create/update relationship(s) among nodes.")
-                        source_node_properties = {}
-                        source_node_properties[relatiionship["target_property_name_in_db"]] = payload[component_id_property_in_payload]
 
-                        target_node_properties = {}
-                        target_node_properties[relatiionship["target_node_property_in_db"]] = payload[relatiionship["target_node_type_id_in_payload"]]
+                        source_node_properties = {} # source node is the node for which the event is currently received
+                        source_node_properties[relationship["source_property_name_in_db"]] = payload[component_id_property_in_payload]
+
+                        target_node_properties = {} # target node is the node which is affect by the the event is currently received
+                        target_node_properties[relationship["target_node_property_in_db"]] = payload[relationship["target_node_type_id_in_payload"]]
 
                         relationship_data = {}
                         relationship_data["source_node_type"] = event_component_type
                         relationship_data["source_node_properties"] = source_node_properties
-                        relationship_data["target_node_type"] = relatiionship['related_node_type']
+                        relationship_data["target_node_type"] = relationship['related_node_type']
                         relationship_data["target_node_properties"] = target_node_properties
-                        relationship_data["relationship"] = relatiionship["relationship"]
-                        relationship_data["relationship_properties"] = relatiionship["relationship_properties"]
+                        relationship_data["relationship"] = relationship["relationship"]
+                        relationship_data["relationship_properties"] = relationship["relationship_properties"]
                         RelationshipManager.create_relationship(relationship_data)
             except KeyError as err:
                 logger.error("KeyError occured while reading key. Error message: {0}".format(str(err)))
